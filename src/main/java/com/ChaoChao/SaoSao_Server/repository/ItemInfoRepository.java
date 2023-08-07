87f599a664a89bbf915ccb73a518d2d927981e53
kwjdgh1267 <78252310+kwjdgh1267@users.noreply.github.com>
2023-08-07 21:07:29 +0900

    Delete src directory

--- src/main/java/com/ChaoChao/SaoSao_Server/repository/ItemInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ChaoChao.SaoSao_Server.repository;
-
-import com.ChaoChao.SaoSao_Server.entity.ItemInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemInfoRepository extends JpaRepository<ItemInfo, Integer> {
-}
--- src/main/java/com/ChaoChao/SaoSao_Server/repository/PurchaseLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ChaoChao.SaoSao_Server.repository;
-
-import com.ChaoChao.SaoSao_Server.entity.PurchaseLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseLogRepository extends JpaRepository<PurchaseLog, Integer> {
-
-}
